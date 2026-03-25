bae7c8644d8a11f09ac3795a3c8a39d2ba4a1bc2
Guilherme Jacobus <guilherme.jacobus@trademe.co.nz>
2026-03-25 23:13:14 +1300

    url fix

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,6 @@
 import type { MetadataRoute } from "next";
 
-const BASE_URL = "https://uppi.com.br";
+const BASE_URL = "https://uppiapp.com.br";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
